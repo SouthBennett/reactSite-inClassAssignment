e73f7b8e929d68f0beec10b9fd7c237b2ac3f714
Connor <connorjohnhughes@gmail.com>
2026-04-14 10:58:37 -0700

    Added Card.jsx

--- react-site/src/App.jsx
+++ react-site/src/App.jsx
@@ -3,12 +3,16 @@
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
 import './App.css'
+import Card from './components/Card';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <h1>yoo</h1>
+    <main>
+        <h1>yoo</h1>
+        <Card />
+    </main>
   )
 }
 
---
+++ react-site/src/components/Card.jsx
@@ -0,0 +1,7 @@
+export default function Card (){
+    return(
+        <h1>
+            This is the card function
+        </h1>
+    )
+}
