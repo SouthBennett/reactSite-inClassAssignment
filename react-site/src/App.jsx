a344377b08d3003c3414fd3cf467b9bbded162c0
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-04-14 10:55:08 -0700

    added Header.jsx and header.css file

--- react-site/src/App.jsx
+++ react-site/src/App.jsx
@@ -2,13 +2,14 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <h1>yoo</h1>
+    <Header />
   )
 }
 
