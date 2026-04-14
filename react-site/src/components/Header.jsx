2f2828de3b0e516503c53c05d42072e2a89c72bc
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-04-14 13:08:23 -0700

    fixed up the css on the header and added anchor tags

--- react-site/src/components/Header.jsx
+++ react-site/src/components/Header.jsx
@@ -2,12 +2,14 @@ import "../components/Header.css"
 
 export default function Header() {
   return (
-    <header>
+    <header className="header">
+      <img src="/img/lort.jpg" alt="lotr-logo" className="nav-logo"/>
+      <h1 className="title">The Free Peoples of Middle-Earth</h1>
+      
       <nav>
-        <img src="/img/lort.jpg" alt="lotr-logo" className="nav-logo"/>
-        <h1>The Free Peoples of Middle-Earth</h1>
+        <a href="#" className="nav-link">Card</a>
+        <a href="#" className="nav-link">Hero</a>
       </nav>
     </header>
-    
-  )
+  );
 }
